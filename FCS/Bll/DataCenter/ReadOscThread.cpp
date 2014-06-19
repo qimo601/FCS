2abe4a28dcceae2075f6f0330ed1b59b4367c057
qimo601 <qimo601@126.com>
2014-06-19 10:10:37 +0800

    提交部分文件

---
+++ FCS/Bll/DataCenter/ReadOscThread.cpp
@@ -0,0 +1,89 @@
+﻿
+#include "ReadOscThread.h"
+#include <QDebug>
+#include "Include/OscDataCenter.h"
+ReadOscThread::ReadOscThread(QObject *parent) :QThread(parent)
+{  
+	stepValue = 0;
+	m_goOn = true;//默认就自动进入循环
+}  
+
+ReadOscThread::~ReadOscThread()  
+{  
+  
+}
+
+void ReadOscThread::run()  
+{ 
+	qDebug() << "【USB监听线程】启动ReadOscThread,线程Id" << QThread::currentThreadId();
+	
+
+	////只要不是停止
+	//while (ctrlTag != CTRL_TAG::STOP_TAG)
+	//{
+
+	//}
+	qint64 step = 0;
+	while (m_goOn)
+	{
+		
+		getOscData();
+		emit oscReadySignal();
+		step++;
+		qDebug() << "【ReadOscThread】step:" <<step;
+		msleep(10);
+	}
+	qDebug() << "【ReadOscThread】运行结束!";
+	this->exec();
+
+}
+
+/**
+* @brief 获取示波器显示数据
+*/
+void ReadOscThread::getOscData()
+{
+	quint8 valueA;
+	quint8 valueB;
+	int value;
+	stepValue = 1;
+
+	//添加至环形缓冲区:示波器一个数据包大小512Byte
+	if (Global::S_CCycleBuffer->getUsedSize() >= 1024 * 16)
+	{
+		OscDataCenter::setLock();
+		OscDataCenter::oscXData.clear();
+		for (int i = 0; i < OscDataCenter::oscYData.count(); i++)
+			OscDataCenter::oscYData[i].clear();
+
+		char * buffer = m_bufferOSC;
+		memset(buffer, 0, 1024 * 16);//清空数组
+		Global::S_CCycleBuffer->read(m_bufferOSC, 1024 * 16);
+		//一次1024组
+		for (int i = 0; i < 1024; i++)
+		{
+			stepValue = i;
+			OscDataCenter::oscXData.append(stepValue);
+			for (int j = 0; j < 8; j++)
+			{
+				memcpy((char *)&valueA, buffer++, 1);
+				memcpy((char *)&valueB, buffer++, 1);
+				//Global::S_CCycleBuffer->read((char *)&valueA, 1);
+				//Global::S_CCycleBuffer->read((char *)&valueB, 1);
+				value = (valueB * 256 + valueA);//高低位
+
+				OscDataCenter::oscYData[j].append(value);
+			}
+
+
+		}
+
+		OscDataCenter::setUnlock();
+	}
+	else//如果没有这个等待，就会有很多空循环，CPU会很高。
+	{
+		return;
+		//Global::S_CCycleBuffer->waitNotEmpty();
+
+	}
+}
---
+++ FCS/Bll/DataCenter/ReadOscThread.h
@@ -0,0 +1,40 @@
+﻿/**
+* 项    目:  FCS软件
+* 文件名称:   ReadOscThread.h
+* 文件路径：  Bll/DataCenter/ReadOscThread.h
+* 摘    要:  示波器读取线程-从循环缓冲区里读取
+* 作    者： 刘兆邦
+* 日    期： 2014年06月15日
+* Copyright (c) 2014-2015, 中科院苏州医工所医学影像室电子组.All rights reserved.
+* Version v1.0
+*/
+
+#ifndef ReadOscThread_H  
+#define ReadOscThread_H
+#include <QThread> 
+#include "Include/Global.h"
+class ReadOscThread : public QThread  
+{  
+	Q_OBJECT
+public:  
+	ReadOscThread(QObject *parent = 0);
+    ~ReadOscThread();  
+    void run(); 
+	void setGoOn(bool on)
+	{
+		m_goOn = on;
+	}
+public slots:
+	/**
+	* @brief 获取示波器显示数据
+	*/
+	void getOscData();
+signals:
+	void oscReadySignal();
+private:
+	double stepValue;
+	char m_bufferOSC[1024 * 16];//一个示波器要显示的数据
+	bool m_goOn;//继续标志
+}; 
+
+#endif
